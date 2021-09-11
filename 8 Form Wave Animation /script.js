c3a914537c7d5c34707cb760539c0960ba9d51f5
Seolcita <seolcita@gmail.com>
2021-09-11 16:12:12 -0600

    With code explanation

--- 8 Form Wave Animation /script.js
+++ 8 Form Wave Animation /script.js
@@ -2,38 +2,46 @@
 
 const labels = document.querySelectorAll('.form-control label');
 
-// labels.forEach((label) => {
-//   label.innerHTML = label.innerText
-//     .split('')
-//     .map(
-//       (letter, idx) =>
-//         `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
-//     )
-//     .join('');
-// });
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join('');
+});
+
+// DETAIL EXPLANATION --------------------------------------------------------------------------
 
 //console.log(labels);
 //In console >> NodeList(2) [label, label] // Eamil, Password
 
-labels.forEach((label) => {
-  const letter = label.innerText.split('');
-  console.log(letter);
+// labels.forEach((label) => {
+//const letter = label.innerText.split('');
+//console.log(letter);
+// In console >> ['E','m','a','i','l'] ['P','a','s','s','w','o','r','d']
+// Split letters are in array called 'letter'
 
-  // In console >> ['E','m','a','i','l'] ['P','a','s','s','w','o','r','d']
-  // Splited letters are in array called 'letter'
+// let letterMap = label.innerText
+//   .split('')
+//   .map((letter) => `<span>${letter}</span>`);
+// console.log(letterMap);
+// In console >> [<span>E</span>,<span>m</span>,<span>a</span>,<span>i</span>,<span>l</span>] // for Email
+// I don't use array for innerHTML. It has to be String. That's why I am using .join()
 
-  label.innerHTML = label.innerText.split('').map((letter) => {
-    `<span>${letter}</span>`;
-  });
-  console.log(label.innerHTML);
-  // In console >> [undefiend,undefiend,undefiend,undefiend,undefiend] ??? // for Email
-  // I don't use array for innerHTML. It has to be String. That's why we are using .join()
+// label.innerHTML = label.innerText
+//   .split('')
+//   .map((letter, index) => `<span>${letter}</span>`)
+//   .join('');
+// console.log(label.innerHTML);
+// I can see <span> is added each letter in html file
+// <label>
+//   <span>E</span>
+//   <span>m</span>
+//   <span>a</span>
+//   <span>i</span>
+//   <span>l</span>
+// </label>
 
-  // label.innerHTML = label.innerText
-  //   .split('')
-  //   .map((letter, index) => {
-  //     `<span>${letter}</span>`;
-  //   })
-  //   .join('');
-  // console.log(label.innerHTML);
-});
+// });
