77c0ac8fe6537c29447462fc5d787ef1e9ffc7d0
Seolcita <seolcita@gmail.com>
2021-10-19 12:45:48 -0600

    25 Sticky Navbar

---
+++ 25 Sticky Navbar/script.js
@@ -0,0 +1,15 @@
+/** @format */
+
+const nav = document.querySelector('.nav');
+const pages = document.querySelectorAll('.nav-item');
+
+// Sticky Nav
+window.addEventListener('scroll', fixNav);
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add('active');
+  } else {
+    nav.classList.remove('active');
+  }
+}
